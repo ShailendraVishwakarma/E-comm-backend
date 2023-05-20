01b1dab9366a7e8cd58a81b1ab19c00df3a3318c
ShailendraVishwakarma <visshailendra@gmail.com>
2023-05-20 19:05:04 +0530

    initial commit

---
+++ db/Product.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+const productSchema=new mongoose.Schema({
+    name:String,
+    price:String,
+    category:String,
+    company:String,
+    userId:String
+    
+
+});
+
+
+
+
+
+module.exports=mongoose.model("products",productSchema);
