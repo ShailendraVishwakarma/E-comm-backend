e3f7b506d80d10abd890e81bcfcc4345a61b5d78
ShailendraVishwakarma <110275464+ShailendraVishwakarma@users.noreply.github.com>
2023-05-22 16:42:31 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,3 +1,17 @@
 const mongoose=require('mongoose') 
-mongoose.connect("mongodb://127.0.0.1:27017/e-commerce",{useNewUrlParser:true,useUnifiedTopology:true},
-    );
+// mongoose.connect("mongodb://127.0.0.1:27017/e-commerce",{useNewUrlParser:true,useUnifiedTopology:true},
+
+ // mongoose.connect("mongodb+srv://visshailendra:root@cluster0.3wxjvr8.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true},
+
+
+    // );
+
+mongoose.connect("mongodb+srv://visshailendra:root@cluster0.3wxjvr8.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true},).then(()=>{
+
+console.log("Connected to DB");
+
+}).catch((err)=>{
+
+console.log(err);
+
+})
